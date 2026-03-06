d287831913645c064f51c96c16c2937fc2c64bc2
prakharDvedi <prakhardwivedi.12@gmail.com>
2026-03-07 01:36:05 +0530

    addded auth/loggin endpoint

--- services/auth-service/src/index.js
+++ services/auth-service/src/index.js
@@ -9,6 +9,7 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcryptjs");
 const users = [];
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
@@ -34,7 +35,23 @@ app.post("/auth/register", async (req, res) => {
   res.status(201).json({ message: "User registered successfully" });
 });
 
-app.post("/auth/login", async (req, res) => {});
+app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = users.find((user) => user.email === email);
+  if (!existingUser) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+  if (existingUser) {
+    const isPassValid = await bcrypt.compare(password, existingUser.password);
+    if (!isPassValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ userId: existingUser.id }, "supersecretkey", {
+      expiresIn: "1h",
+    });
+    return res.status(200).json({ token: token });
+  }
+});
 
 app.listen(3001, () => {
   console.log("Server started on port 3001");
